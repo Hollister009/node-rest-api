09c844d5ec73e94501f4a97ff07b1486981e3b38
Hollister009 <hipnos.az@gmail.com>
2019-08-21 13:41:59 +0300

    Improved server.js and router.js

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello User!'));
 
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const router = require('./router');
 const express = require('express');
 const path = require('path');
 
-router.use(express.static(path.join(__dirname, 'src')));
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(router);
 
 const port = process.env.PORT || 3000;
 
-router.listen(port, () => console.log(`Listening on ${port}!`));
+app.listen(port, () => console.log(`Listening on ${port}!`));
