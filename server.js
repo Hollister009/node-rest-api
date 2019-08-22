583d61376038cba5ed363bc73261a0c8790518fe
Hollister009 <hipnos.az@gmail.com>
2019-08-22 15:27:29 +0300

    Database connection and dotenv package

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const router = require('./api/router');
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = require('./api/router');
+require('dotenv').config();
 
 const app = express();
 const rootPath = path.resolve('api');
 
-app.use(express.static(rootPath));
-app.use(router);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('connected to database'));
+
+app
+  .use(router)
+  .use(express.json())
+  .use(express.static(rootPath));
 
 const port = process.env.PORT || 3000;
 
