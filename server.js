b07049b27b7c2e87b99d348a02ba5a535aa10ded
Hollister009 <hipnos.az@gmail.com>
2019-08-27 11:29:25 +0300

    server.js cleanup

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
-const router = require('./api/router');
 require('dotenv').config();
 
 const app = express();
+const router = require('./api/router');
 const rootPath = path.resolve('api');
+const port = process.env.PORT || 3000;
+const db_uri = process.env.MONGODB_URI;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(db_uri, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
-db.once('open', () => console.log('connected to database'));
+db.once('open', () => console.log('MongoDB connection established'));
 
 const loggerTool = (req, res, next) => {
   const now = new Date().toLocaleString();
@@ -28,12 +29,9 @@ const loggerTool = (req, res, next) => {
 
 //Request Parsing
 app
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
+  .use(express.json())
   .use(express.static(rootPath))
   .use(loggerTool)
   .use(router);
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
