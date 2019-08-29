8427f3ae4b4d64fda788f76eb61f2b9ba768436e
Hollister009 <hipnos.az@gmail.com>
2019-08-29 16:34:03 +0300

    Error handling middleware

--- server.js
+++ server.js
@@ -36,4 +36,17 @@ app
   .use(loggerTool)
   .use(router);
 
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message
+  });
+})
+
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
